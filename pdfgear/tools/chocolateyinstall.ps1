edf15c4809f3ea5494566d508706cf8847e888ce
HeckmanK <Kevin@KevinHeckman.com>
2024-04-19 22:28:31 -0400

    Initial Commit

---
+++ pdfgear/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64      = 'https://www.pdfgear.com/download/pdfgear_setup_v2.1.5.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url64bit      = $url64
+  softwareName  = 'pdfgear*'
+  checksum64    = 'DB1E3B9F03008745A59FF36D6DA30BCC5DC496AED12B65909632D142C4357359'
+  checksumType64= 'sha256'
+
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
