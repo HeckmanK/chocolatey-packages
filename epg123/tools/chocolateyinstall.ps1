c469a2dd511bc31e07abea312f82d4cbe958b191
HeckmanK <Kevin@KevinHeckman.com>
2024-05-01 16:12:34 -0400

    Initial commit

---
+++ epg123/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileFullPath = Join-Path $toolsDir 'epg123Setup_v1.8.2.11.zip'
+
+Get-ChocolateyUnzip -FileFullPath $fileFullPath -destination $toolsDir
+$fileLocation = (Get-ChildItem -Path $toolsDir -Filter "epg123*.exe").FullName
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'EXE'
+  file         = $fileLocation
+  softwareName  = 'epg123*'
+  validExitCodes= @(0, 3010, 1641)
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+}
+
+Install-ChocolateyInstallPackage @packageArgs
