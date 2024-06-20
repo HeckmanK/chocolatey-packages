6d314e40ae962d2eff9f441b77661549af81cc42
HeckmanK <167263555+HeckmanK@users.noreply.github.com>
2024-06-20 17:52:59 -0400

    Added files for FreeOCR

---
+++ freeocr/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'freeocr541.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'EXE'
+  file         = $fileLocation
+  softwareName  = 'freeocr*' 
+  validExitCodes= @(0, 3010, 1641)
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+}
+
+Install-ChocolateyInstallPackage @packageArgs
