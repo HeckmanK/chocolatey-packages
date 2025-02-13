ac1840c6dbece494e001e57192db5e0b67e3c765
HeckmanK <167263555+HeckmanK@users.noreply.github.com>
2025-02-12 21:14:39 -0600

    added interCAD Reader Free

---
+++ intercad-reader-free/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.intercad.com/member/download.jsp?dir=/file/program_download&Indx=4'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'interCAD Reader*'
+  checksum      = '1B9E6D68A9F56E89C8B6F9D1FBF757BF87F3843AB54C9554C1456C788333FA71'
+  checksumType  = 'sha256'
+  silentArgs   = '/s /v"/qn"'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
