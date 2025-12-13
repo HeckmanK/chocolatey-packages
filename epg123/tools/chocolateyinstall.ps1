a2b5225b9bbcb1e6c45aa823eadbcef7461f1f12
HeckmanK <167263555+HeckmanK@users.noreply.github.com>
2025-12-12 21:25:24 -0600

    updated epg123 to v1.8.4.3

--- epg123/tools/chocolateyinstall.ps1
+++ epg123/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileFullPath = Join-Path $toolsDir 'epg123Setup_v1.8.4.2.zip'
+$fileFullPath = Join-Path $toolsDir 'epg123Setup_v1.8.4.3.zip'
 
 Get-ChocolateyUnzip -FileFullPath $fileFullPath -destination $toolsDir
 $fileLocation = (Get-ChildItem -Path $toolsDir -Filter "epg123*.exe").FullName
