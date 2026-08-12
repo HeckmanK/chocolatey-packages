b756c1af034fbf6252638f186c33ea6f3371ec73
HeckmanK <167263555+HeckmanK@users.noreply.github.com>
2026-08-11 21:10:22 -0500

    updated pdfgear to v2.1.19

--- pdfgear/tools/chocolateyinstall.ps1
+++ pdfgear/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://downloadfiles.pdfgear.com/releases/windows/pdfgear_setup_v2.1.15.exe'
+$url64      = 'https://downloadfiles.pdfgear.com/releases/windows/pdfgear_setup_v2.1.19.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -8,7 +8,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   url64bit      = $url64
   softwareName  = 'pdfgear*'
-  checksum64    = '3a37578a08e0723b1bd101482c9e3cfa8f4add4f8854ec0187fc5cf2bfef9fe2'
+  checksum64    = '2f9c2548233f54ea58c7946accef44b8714f1e23517deaa3249f7286a122639b'
   checksumType64= 'sha256'
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0)
